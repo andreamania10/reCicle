50e00c6fd67356a853934f60ff077ec6298bf751
Andrea Martinez <andrea.martinez@raona.com>
2026-06-06 18:31:53 +0200

    components home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,20 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/home/home';
+import { LoginModalComponent } from './components/login-modal/login-modal';
+import { RegisterComponent } from './components/register/register';
+
+// Afegeix aquests nous després
+// import { ProfileComponent } from './components/profile/profile';
+// import { AddArticleComponent } from './components/add-article/add-article';
+// import { CategoriesComponent } from './components/categories/categories';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginModalComponent },
+  { path: 'register', component: RegisterComponent },
+
+  // Per fer avui 👇
+  // { path: 'profile', component: ProfileComponent },
+  // { path: 'add', component: AddArticleComponent },
+  // { path: 'categories', component: CategoriesComponent }
 ];
---
+++ src/app/components/add-article/add-article.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-article',
+  imports: [],
+  templateUrl: './add-article.html',
+  styleUrl: './add-article.css',
+})
+export class AddArticle {
+
+}
