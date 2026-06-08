d2ee7104a12c7e4686a704125bd797649ac022a2
Andrea Martinez <andrea.martinez@raona.com>
2026-06-08 17:01:18 +0200

    home configurable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/landing/landing').then(m => m.Landing) },
+  { path: '', loadComponent: () => import('./pages/home/home').then(m => m.Home) },
+  // { path: '', loadComponent: () => import('./pages/landing/landing').then(m => m.Landing) },
   { path: 'home', loadComponent: () => import('./pages/home/home').then(m => m.Home), /*canActivate: [authGuard]*/ },
   { path: 'login', loadComponent: () => import('./pages/login/login').then(m => m.Login) },
   { path: 'register', loadComponent: () => import('./pages/register/register').then(m => m.Register) },
