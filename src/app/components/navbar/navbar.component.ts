a793b7b4ac8f062759b6c4ca92b6e58214d77b5e
Andrea Martinez <andrea.martinez@raona.com>
2026-06-19 20:01:20 +0200

    navbar change pages and profile logic with picture from profile

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../../interfaces/user';
+import { UserService } from '../../services/user';
 import { Router, RouterLink } from '@angular/router';
 import { RegisterComponent } from '../register/register.component';
+
 import { Auth } from '../../services/auth';
-
 declare var bootstrap: any;
-
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -13,18 +14,39 @@ declare var bootstrap: any;
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   @Input() showLoginButton = false;
   isMenuOpen = false;
+  profile: User | null = null;
 
   constructor(
+    // private userService: UserService,
     readonly auth: Auth,
-    private router: Router,
+    private router: Router
   ) {}
+
+  ngOnInit(): void {
+    this.auth.isLoggedIn();
+  
+    const userId = this.auth.currentUser()?.id;
+    if (!userId) return;
+  
+    // this.userService.getById(userId).subscribe({
+    //   next: (user: User) => {
+    //     this.profile = user;
+    //   },
+    //   error: (err: unknown) => {
+    //     console.error('Error carregant perfil al navbar', err);
+    //   }
+    // });
+  }
 
   get homeLink(): string {
     return this.auth.currentUser() ? '/home' : '/';
   }
+  // get userInitial(): string {
+  //   return this.profile?.username?.charAt(0).toUpperCase() ?? '?';
+  // }
 
   openLoginModal(): void {
     const modalEl = document.getElementById('loginModal');
@@ -35,5 +57,9 @@ export class NavbarComponent {
 
   goToProfile(userId: number): void {
     this.router.navigate(['/profile', userId]);
+  }
+
+  sellProducts(){
+    this.router.navigate(['/sellProduct'])
   }
 }
